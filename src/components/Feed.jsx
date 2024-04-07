f62390730a34aac60a9f486d337641ae75f7cb7b
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2024-04-07 16:33:43 +0530

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,23 +1,3 @@
-hy>
-//     </Box>
-//     <Box p={2} sx={{overflow:'auto',height:'90vh',flex:2}}>
-//     <Typography variant="h4"
-//     fontWeight="bold" mb={2} sx={{
-//         color:'white'
-//     }} >
-//     {selectedCategory} <span style={{
-//         color:'#F31503'
-//     }}>videos</span>
-     
-//     </Typography>
-//     <Videos videos={videos} />
-//     </Box>
-//     </Stack>
-//   )
-// }
-
-// export default Feed
-
 
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
