9d4ba52ab1bcdba1d013bf2969d854eca503bd0e
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2024-04-07 16:33:27 +0530

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,19 +1,4 @@
-
-//     sx={{flexDirection:{sx:'column',md:'row'}}}>
-//     <Box sx={{
-//         height:{sx:'auto',md:'92vh'},
-//         borderRight:'1px solid #3d3d3d',
-//         px:{sx:0,md:2}
-//     }}>
-//    <Sidebar
-//     selectedCategory={selectedCategory}
-//     setSelectedCategory={setSelectedCategory}
-
-//    />
-//     <Typography className='copyright' variant='body2'
-//     sx={{mt:1.5,color:'#fff'}}>
-//         Copyright 2024 
-//     </Typography>
+hy>
 //     </Box>
 //     <Box p={2} sx={{overflow:'auto',height:'90vh',flex:2}}>
 //     <Typography variant="h4"
