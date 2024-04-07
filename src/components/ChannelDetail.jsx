6d5a1ff015bf56487d74ef2b2c9b0083f5b544b0
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2024-04-07 16:32:33 +0530

    Update ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,20 +1,4 @@
-// import {useState,useEffect} from 'react';
-// import {useParams} from 'react-router-dom';
-// import {Box} from '@mui/material';
-// import {Videos,ChannelCard} from './';
-// import { fetchFromAPI } from '../utils/fetchFromAPI';
 
-// const ChannelDetail = () => {
-//     const [channelDetail, setChannelDetail] = useState(null); 
-//     const {id}=useParams();
-//     console.log(channelDetail)
-//     useEffect(()=>{
-//         fetchFromAPI(`channels?part="snippet&id=${id}`)
-//         .then((data)=>setChannelDetail(data?.items[0]));
-//     },[id])
-//   return (
-//     <div>ChannelDetail</div>
-//   )
 // }
 
 // export default ChannelDetail;
